4fecf1f28d06a0910bad5f69a0fff964630d5a3e
Richtofen4 <dawid.ciurkot@interia.pl>
2025-11-14 14:10:33 +0100

    Frontend2

--- src/fetches/expandLink.jsx
+++ src/fetches/expandLink.jsx
@@ -1,5 +1,12 @@
-// src/fetches/expandLink.js
+/*
 export function expandLink(p) {
-  const base = import.meta.env.VITE_API_BASE ?? ''; // w prod zazwyczaj ''
+  const base = import.meta.env.VITE_API_BASE ?? '';
   return `${base}${p}`;
+}
+*/
+const API_BASE =
+  import.meta.env.VITE_API_BASE || "http://localhost:4000";
+
+export function expandLink(path) {
+  return API_BASE + path;
 }
